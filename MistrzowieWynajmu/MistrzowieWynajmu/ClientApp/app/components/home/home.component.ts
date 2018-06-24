7c1571845e7940455d212bd498630168bd7b8b81
ThePragmaticTendencies <adam.bawolek@gmail.com>
2018-06-24 19:46:22 +0200

    Repo z kursu

--- MistrzowieWynajmu/MistrzowieWynajmu/ClientApp/app/components/home/home.component.ts
+++ MistrzowieWynajmu/MistrzowieWynajmu/ClientApp/app/components/home/home.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+	title: string = "to jest title z komponentu.";
+	isCool: boolean = true;
+	year: number = 2017;
 }
